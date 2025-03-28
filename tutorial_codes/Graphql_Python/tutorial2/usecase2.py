8ff45c083162677f34076ee3ff4ea7a0056c90f9
TheMadrasTechie <techiemadras@gmail.com>
2025-03-28 20:45:36 +0530

    did something with uscases

--- tutorial_codes/Graphql_Python/tutorial2/usecase2.py
+++ tutorial_codes/Graphql_Python/tutorial2/usecase2.py
@@ -1,41 +1,35 @@
 import graphene
 from fastapi import FastAPI
-from graphene.types.objecttype import ObjectType
-from starlette.graphql import GraphQLApp
-from graphene import ObjectType as ot
-from graphene import String as st
-from graphene import Int as int
-from graphene import List as li
+from starlette_graphene3 import GraphQLApp, make_graphiql_handler
 
-data=[
-    {
-        "name": "Roni",
-        "city": "Cologne",
-        "country": "India"
-
-},
-{
-        "name": "John",
-        "city": "London",
-        "country": "UK"
-
-},
-{
-         "name": "Maria",
-         "city": "Sydney",
-         "country": "Australia"
-}
+# Sample data
+data = [
+    {"name": "Roni", "city": "Cologne", "country": "India","number":982882},
+    {"name": "John", "city": "London", "country": "UK"},
+    {"name": "Maria", "city": "Sydney", "country": "Australia"}
 ]
 
-class students(ot):
-    name=st()
-    city=st()
-    country=st()
 
-class person(ot):
-    student=li(students)
-    def resolve_student(self,info):
+
+# GraphQL Type for Student
+class Student(graphene.ObjectType):
+    name = graphene.String()
+    city = graphene.String()
+    country = graphene.String()
+    number = graphene.Int()
+    name = graphene.String()
+# Root Query
+class Query(graphene.ObjectType):
+    students = graphene.List(Student)
+    
+    def resolve_students(self, info):
         return data
-app=FastAPI()
-app.add_route("/graphql",GraphQLApp(schema=graphene.Schema(query=person)))
-print(graphene.Schema(query=person))
+
+# Create FastAPI app
+app = FastAPI()
+
+# Create schema and GraphQL app with GraphiQL UI enabled
+schema = graphene.Schema(query=Query)
+app.add_route("/graphql", GraphQLApp(schema=schema, on_get=make_graphiql_handler()))
+
+print(schema)
