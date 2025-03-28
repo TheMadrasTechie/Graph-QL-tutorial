96ef8235ef4516d36e6b7541a2b613a764262d33
TheMadrasTechie <techiemadras@gmail.com>
2025-03-28 22:40:36 +0530

    did two new ways to fix

--- tutorial_codes/Graphql_Python/tutorial8/main.py
+++ tutorial_codes/Graphql_Python/tutorial8/main.py
@@ -1,8 +1,18 @@
 import graphene
 from fastapi import FastAPI
-from starlette.graphql import GraphQLApp
-from mapping import query
+from starlette_graphene3 import GraphQLApp, make_graphiql_handler
+from mapping import query  # This should be a graphene.ObjectType in mapping.py
 
-app=FastAPI()
-app.add_route("/graphql",GraphQLApp(schema=graphene.Schema(query=query)))
-print(graphene.Schema(query=query))
+app = FastAPI()
+
+# Define the GraphQL schema
+schema = graphene.Schema(query=query)
+
+# Create the GraphQL route with GraphiQL UI enabled
+graphql_app = GraphQLApp(schema=schema, on_get=make_graphiql_handler())
+
+# Mount the /graphql route
+app.add_route("/graphql", graphql_app)
+
+# Optional: print the schema for debug
+print(schema)
