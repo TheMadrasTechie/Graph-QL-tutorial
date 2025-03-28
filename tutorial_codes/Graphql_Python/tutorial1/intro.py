2ad41b7fcf3935f88ade6a6be60785e3277b9b5f
TheMadrasTechie <techiemadras@gmail.com>
2025-03-28 20:12:28 +0530

    did the basics in graphql

--- tutorial_codes/Graphql_Python/tutorial1/intro.py
+++ tutorial_codes/Graphql_Python/tutorial1/intro.py
@@ -1,18 +1,25 @@
 from fastapi import FastAPI
 import graphene
-from starlette.graphql import GraphQLApp
+from starlette_graphene3 import GraphQLApp, make_graphiql_handler
 
 
-class calculator(graphene.ObjectType):
-    concat=graphene.String(a=graphene.String(),b=graphene.String())
-    add=graphene.String(a=graphene.Int(),b=graphene.Int())
-    def resolve_concat(self,info,a,b):
-        #return a+" "+b
-        return "concatination works"
-    def resolve_add(self,info,a,b):
-        #return a+b
-        return "addition works"
+class Calculator(graphene.ObjectType):
+    concat = graphene.String(a=graphene.String(), b=graphene.String())
+    add = graphene.String(a=graphene.Int(), b=graphene.Int())
 
-app=FastAPI()
-app.add_route("/",GraphQLApp(schema=graphene.Schema(query=calculator)))
-    
+    def resolve_concat(self, info, a, b):
+        return a +" "+b
+
+    def resolve_add(self, info, a, b):
+        return a+b
+
+
+app = FastAPI()
+
+schema = graphene.Schema(query=Calculator)
+
+# Enable GraphiQL
+graphql_app = GraphQLApp(schema=schema, on_get=make_graphiql_handler())
+
+# Mount GraphQL at /graphql
+app.add_route("/graphql", graphql_app)
