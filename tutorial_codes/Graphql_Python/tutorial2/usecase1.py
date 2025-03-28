77990b0746e545c2372ab2d92842fe4907b05f08
TheMadrasTechie <techiemadras@gmail.com>
2025-03-28 20:30:00 +0530

    my new case done

--- tutorial_codes/Graphql_Python/tutorial2/usecase1.py
+++ tutorial_codes/Graphql_Python/tutorial2/usecase1.py
@@ -1,15 +1,35 @@
 import graphene
 from fastapi import FastAPI
-from starlette.graphql import GraphQLApp
-course_name="computer sience"
-course_time_year=1
-class course(graphene.ObjectType):
-    name= graphene.String()
-    duration=graphene.Int()
-    def resolve_name(self,info):
+from starlette_graphene3 import GraphQLApp, make_graphiql_handler
+
+# Sample data
+course_name = "Computer Science"
+course_time_year = 1
+sundar_city = "Tiruppur"
+
+# Define GraphQL schema
+class Course(graphene.ObjectType):
+    name = graphene.String()
+    duration = graphene.Int()
+    city = graphene.String()
+    def resolve_name(self, info):
         return course_name
-    def resolve_duration(self,into):
+
+    def resolve_duration(self, info):
         return course_time_year
-app=FastAPI()
-app.add_route("/graphql",GraphQLApp(schema=graphene.Schema(query=course)))
-print(graphene.Schema(query=course))
+    
+    def resolve_city(self, info):
+        return sundar_city
+
+# Create FastAPI app
+app = FastAPI()
+
+# Define GraphQL schema and enable GraphiQL UI
+schema = graphene.Schema(query=Course)
+graphql_app = GraphQLApp(schema=schema, on_get=make_graphiql_handler())
+
+# Add route
+app.add_route("/graphql", graphql_app)
+
+# Optional: print schema (for debugging)
+print(schema)
