f33fdbadbc2365440b21af007513c768d20e25aa
TheMadrasTechie <techiemadras@gmail.com>
2025-03-28 22:11:10 +0530

    did the documnetation

--- tutorial_codes/Graphql_Python/tutorial6/main.py
+++ tutorial_codes/Graphql_Python/tutorial6/main.py
@@ -1,18 +1,33 @@
 import graphene
 from fastapi import FastAPI
-from starlette.graphql import GraphQLApp
+from starlette_graphene3 import GraphQLApp, make_graphiql_handler
 
-class myquery(graphene.ObjectType):
+# Define GraphQL Query
+class MyQuery(graphene.ObjectType):
     class Meta:
-        name="typefrommetaclass1"
-        description="description from metaclass1"
-    hello=graphene.String(description="hello string")
-    bye=graphene.String(description="bye string")
-    def resolve_hello(self,info):
-        
+        name = "typefrommetaclass1"
+        description = "description from metaclass1"
+
+    hello = graphene.String(description="hello string")
+    bye = graphene.String(description="bye string")
+
+    def resolve_hello(self, info):
         return "hello world!!!"
-    def resolve_bye(self,info):
+
+    def resolve_bye(self, info):
         return "good bye!!"
-app=FastAPI()
-app.add_route("/graphql",GraphQLApp(schema=graphene.Schema(query=myquery)))
-print(graphene.Schema(query=myquery))
+
+# Create FastAPI app
+app = FastAPI()
+
+# Define schema
+schema = graphene.Schema(query=MyQuery)
+
+# Create GraphQLApp with GraphiQL UI enabled
+graphql_app = GraphQLApp(schema=schema, on_get=make_graphiql_handler())
+
+# Add route
+app.add_route("/graphql", graphql_app)
+
+# Optional debug print
+print(schema)
